8a20097d3d107450758e294d518adccc0bad38ca
Yenyi5 <yenyi.wu93@gmail.com>
2024-12-11 18:27:27 +0100

    Add files via upload

--- main_dataverse.py
+++ main_dataverse.py
@@ -14,7 +14,7 @@ import random
 import pickle
 from urllib.parse import quote
 
-### parameters ###
+# Define key parameters for the script
 page_size = 200
 n_desired_records = 500  # Number of desired records
 # URL for Harvard Dataverse API platform
@@ -22,7 +22,6 @@ api_key = '2a8faeaf-d10d-401c-8977-3b193d3be3be'
 header_name={'X-Dataverse-key': '2a8faeaf-d10d-401c-8977-3b193d3be3be'}
 API_URL = f"https://dataverse.harvard.edu/api/search"
 
-##################
 
 # query keys that are serially searched for
 query_list = ['geospatial', 'gis', 'remote sensing', 'ISO 19115', 'ISO 19119', 'ISO 19139', 'shapefile', 'geodatabase', 'vector', 'raster']
@@ -30,7 +29,7 @@ query_list = ['geospatial', 'gis', 'remote sensing', 'ISO 19115', 'ISO 19119', '
 # file formats for filtering out relevant search results
 geospatial_format_list = ['.shp', '.geojson', '.kml','.gml', '.asc', '.tif', '.tiff', '.img','.rst', '.gdb']
 
-# Define the standard keys you want to include in your CSV
+# Keys to include in the saved records
 standard_save_keys = ['name', 'description', 'published_at', 'identifier', 'latestVersion', 'fileCount', 'totalSize']
 
 def get_total_pages(dataverse_url, query_string, size, retries=3):
@@ -60,12 +59,12 @@ def get_article_files(metadata):
     # Get the persistent_id from the metadata
     persistent_id = metadata['global_id']  # Assuming this contains the full DOI, e.g., 'doi:10.7910/DVN/68IYCX'
     
+    # Construct an URL to fetch article files using article persistent IDs
     url_constructed= f"{api_url}/datasets/export?exporter=dataverse_json&persistentId={quote(persistent_id)}"
-    # [requests.get(url) for i in range(200)]
-    #print(f"Fetching files from: {url12345}")
+    # debug: [requests.get(url) for i in range(200)]
+    
     response = requests.get(url=url_constructed, headers=header_name)
-    #print(response.headers)
-    #print(f"Response status:< {response.status_code}")
+    # debug: print(f"Response status:< {response.status_code}")
     
     if response.status_code == 200:
         data = response.json()
@@ -73,7 +72,7 @@ def get_article_files(metadata):
         # data['datasetVersion']['files'][0]['dataFile']['filename'] # oder filesize
         return data['datasetVersion']['files'] # Liste mit file dicts
         # darin enthalten : dict_keys(['description', 'label', 'restricted', 'version', 'datasetVersionId', 'dataFile'])
-        # in ['dataFile'] stecken relevante infos : 'filename' und 'filesize'
+        # in ['dataFile'] relevant infos : 'filename' und 'filesize'
     else:
         print(f"Error fetching files: {response.status_code} - {response.text}")
         return []
@@ -87,37 +86,28 @@ def fetch_records(dataverse_url, query_string, page_number, size, api_key, retri
         response = requests.get(dataverse_url, params={'q': query_string, 'start': page_number * size, 'per_page': size}, headers={'X-Dataverse-key': '2a8faeaf-d10d-401c-8977-3b193d3be3be'}, timeout=30)
         response.raise_for_status()
         metadata_list = response.json()['data']['items'] 
-        # Filtern der metadata_list : files müssen aussortiert werden, da bereits in den datensets enthalten
         metadata_list = [metadata_list[index] for index in range(len(metadata_list))  if metadata_list[index]['type'] == 'dataset' ]
-        # metadata_list : Einträge müssen einzeln durchgegangen werden und mit 'id' key die zugehörigen Files abgerufen werden
         update_metadata_list = list()
         for index, metadata in enumerate(metadata_list):#[:10]:
             try:
-                # hier mit get_article_files() die files zu den metadaten runterladen
                 if 'global_id' in metadata.keys():
                     data = get_article_files(metadata) # file format info in "name" key
                 else:
                     data = []
-                    #print(index, 'no global_id found')
                 # data keys: 'id', 'name', 'size', 'is_link_only', 'download_url', 'supplied_md5', 'computed_md5', 'mimetype'
-                if len(data)>0: # if metadata contains data, then get file information and add to dict
-                   
+                # if metadata contains data, then get file information and add to dictionary
+                if len(data)>0: 
                     single_data_dict = {}
                     # Iterate over each dictionary in the list
                     for dictionary in data:
                         for key, value in dictionary['dataFile'].items():
                             # Add values to the list corresponding to each key in the result dictionary
                             single_data_dict.setdefault(key, []).append(value)
-                                                                                                                                    
+
                     # Rename 'id' from datafile to 'id_file' to distinguish between metadata id and file ids
                     if 'description' in single_data_dict.keys():
                         single_data_dict['file_description'] = single_data_dict.pop('description')
-
-                    # file keys die hinzugefügt werden : ['id', 'persistentId', 'pidURL', 'filename', 'contentType', 'filesize', 'description', 'storageIdentifier', 'rootDataFileId', 'md5', 'checksum', 'creationDate']
                     metadata.update(single_data_dict)
-                    # metadata geupdatet keys: 
-                        # 'id', 'title', 'doi', 'handle', 'url', 'published_date', 'thumb', 'defined_type', 'defined_type_name', 'group_id', 'url_private_api', 'url_public_api', 'url_private_html', 'url_public_html', 'timeline', 'resource_title', 'resource_doi', 'name', 'size', 'is_link_only', 'download_url', 'supplied_md5', 'computed_md5', 'mimetype'
-                    # geupdatete metadata (einzelner update step) werden wieder in eine list zusammengesammelt
                 update_metadata_list.append(metadata) 
                 
             except  requests.exceptions.RequestException as e:
